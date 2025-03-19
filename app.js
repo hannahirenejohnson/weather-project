9079372b51110cd2cf5ce5bc61823ec1736933d0
hannahirenejohnson <hannahirenejohnson@users.noreply.github.com>
2025-03-18 23:37:45 -0400

    Added Dark Theme Button & CSS classes

---
+++ app.js
@@ -0,0 +1,11 @@
+function darkTheme() {
+    // let makeItDark = document.querySelector(".container"); // Select the container
+    // makeItDark.classList.toggle("dark"); // Toggle the "dark" class on the container
+
+    // OR if you meant to apply it to the entire page:
+    document.body.classList.toggle("dark");
+}
+
+let button = document.querySelector(".dark-theme-button"); // Target only the dark mode button
+button.addEventListener("click", darkTheme);
+
